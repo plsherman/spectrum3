0ca029f57c7debe24e3c303a4d0547da54941f2f
Philip Sherman <plsherman@att.net>
2025-12-18 23:27:10 -0500

    change output to jpg, add addditional parm for display time

--- cam1.py
+++ cam1.py
@@ -21,10 +21,10 @@ roi="--roi=0.3445,0.3717,0.37172,0.26382"	#801,912,1521,1140	x,y,xLength,yLength
 fn1 = "-o"
 fn2=""
 expTime ="--shutter="
-encoding="--encoding=yuv420"		# yuv420, jpg & others
+encoding="--encoding=jpg"		# yuv420, jpg & others
 denoise ="--denoise=off"
 quality ="--quality=100"		# jpg only
-timeout ="--timeout=1sec"		# how long to show image
+timeout ="--timeout=3sec"		# how long to show image
 awb     ="--awb=daylight"		# daylight, auto, many others including custom
 numParms= len(sys.argv)
 
@@ -33,6 +33,8 @@ if numParms > 1:
 else:
   fn2 = fnDefault
   print("No parameters passed - using",fnDefault)
+  print("  Parms: fn.JPG   nnnnExposureInUS  nnDisplayTimeSec  TRACE|trace")
+  print(" Dflts: test3.jpg  300000               15")
 # 			exposure time specified in microseconds (us)
 
 if numParms > 2:
@@ -40,7 +42,9 @@ if numParms > 2:
 else:
   expTime = expTime+str(expTimeDefault)
   print("No time parameter passed, using default:",expTimeDefault,"microseconds")
-if numParms >3:
+if numParms > 3:
+  timeout = "--timeout="+sys.argv[3]+"sec"
+if numParms > 4:
   tracer = True
 
 if tracer:
