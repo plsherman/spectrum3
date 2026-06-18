f5cb46aae3790fa065d61e1294ab806e18171d46
Philip Sherman <plsherman@att.net>
2026-06-18 00:46:36 -0400

    correct script to run v4 spectrum program

--- bin/GITspectrum4.sh
+++ bin/GITspectrum4.sh
@@ -1,5 +1,5 @@
-cd /home/spectrum/gitRepos/Spectrum3
-pi4j -r Spectrum4
-# echo ""
-# echo ""
+cd /home/spectrum/gitRepos/spectrum4
+pi4j -r Spectrum4 trace trace1 trace3 | tee lastRun.trace
+echo ""
+echo ""
 # read -p "Press [Enter] to close this window" dumy1
